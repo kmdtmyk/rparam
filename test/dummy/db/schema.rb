dee3844dbd3ef3d5a74cfcb39bfe020c12e4c5c6
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-01-07 18:22:12 +0900

    Create user table

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_07_035925) do
+ActiveRecord::Schema.define(version: 2019_01_07_091753) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.date "release_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
