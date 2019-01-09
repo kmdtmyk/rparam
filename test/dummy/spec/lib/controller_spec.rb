edcd22dc246802fa9b915c6277124f736d9b9d36
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-01-09 16:03:52 +0900

    Add test of controller

--- test/dummy/spec/lib/controller_spec.rb
+++ test/dummy/spec/lib/controller_spec.rb
@@ -9,12 +9,60 @@ RSpec.describe TestsController, type: :controller do
 
   describe 'apply_each_rparam' do
 
+    describe 'inclusion' do
+
+      example do
+        get :index, params: { order: 'asc' }
+        @controller.apply_each_rparam :order, inclusion: %w(asc desc)
+        expect(@controller.params[:order]).to eq 'asc'
+
+        get :index, params: { order: 'foo' }
+        @controller.apply_each_rparam :order, inclusion: %w(asc desc)
+        expect(@controller.params[:order]).to eq nil
+      end
+
+      describe 'with default value' do
+
+        example do
+          get :index, params: { order: 'asc' }
+          @controller.apply_each_rparam :order, inclusion: %w(asc desc), default: 'asc'
+          expect(@controller.params[:order]).to eq 'asc'
+
+          get :index, params: { order: 'foo' }
+          @controller.apply_each_rparam :order, inclusion: %w(asc desc), default: 'asc'
+          expect(@controller.params[:order]).to eq 'asc'
+        end
+
+      end
+
+    end
+
     describe 'type: Date' do
 
-      it do
+      example do
         get :index, params: { date: '2018-12-15' }
         @controller.apply_each_rparam :date, type: Date
         expect(@controller.params[:date]).to eq Date.new(2018, 12, 15)
+
+        get :index, params: { date: 'invalid' }
+        @controller.apply_each_rparam :date, type: Date
+        expect(@controller.params[:date]).to eq nil
+      end
+
+    end
+
+    describe 'save: true' do
+
+      example do
+        get :index, params: { order: 'asc' }
+        @controller.apply_each_rparam :order, save: true
+        expect(@controller.params[:order]).to eq 'asc'
+        expect(ControllerParameter.count).to eq 1
+
+        get :index
+        @controller.apply_each_rparam :order, save: true
+        expect(@controller.params[:order]).to eq 'asc'
+        expect(ControllerParameter.count).to eq 1
       end
 
     end
