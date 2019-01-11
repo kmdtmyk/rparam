b57c4dcd2b09eb2f2917bd8beaf99f027bd03c5c
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-01-11 21:49:50 +0900

    Change value of save memory

--- lib/rparam/controller.rb
+++ lib/rparam/controller.rb
@@ -19,123 +19,54 @@ module Rparam
         return unless parameter.respond_to? action_name
         # return unless parameter.method(action_name).owner == parameter.class
         parameter.send action_name
+
+        calculator = Calculator.new(params, rparam_memory)
+
         parameter.each do |name, options|
-          apply_each_rparam(name, options)
+          calculator.add name, options
         end
+
+        params.merge! calculator.result
+        save_rparam_memory calculator.memory
       end
 
-      def apply_each_rparam(name, options)
-        options ||= {}
-        value = params[name]
-
-        if options[:save].present?
-          if value.nil?
-            value = load_parameter(name, options)
-          else
-            save_parameter(name, value, options)
-          end
-        end
-
-        if options[:inclusion].present?
-          inclusion = Array.wrap options[:inclusion]
-          if value.is_a? Array
-            value = value & inclusion
-          else
-            value = nil unless value.in? inclusion
-          end
-        end
-
-        if options.has_key? :exclusion
-          exclusion = Array.wrap options[:exclusion]
-          if value.is_a? Array
-            value = value - exclusion
-          else
-            value = nil if value.in? exclusion
-          end
-        end
-
-        if options[:default].present?
-          if options[:save] == :relative_date
-            value = options[:default] if value.blank?
-          elsif value.nil?
-            value = options[:default]
-          end
-        end
-
-        if options[:type].present?
-          value = Parser.parse(value, options[:type])
-        end
-
-        if value.nil? and options[:type] == Array
-          value = []
-        end
-
-        params[name] = value
-      end
-
-      def save_parameter(name, value, options)
-        if options[:save] == :relative_date
-          date = Parser.parse_date(value)
-          if date.nil?
-            value = nil
-          else
-            value = (date - Time.zone.today).to_i
-          end
-        end
+      def rparam_memory
         user = current_rparam_user
         if user.nil?
-          save_parameter_to_cookie(name, value)
-        else
-          rparam_memory = user.rparam_memories.find_or_create_by(
-            action: full_action_name,
-            name: name,
-          )
-          rparam_memory.update(value: value)
-        end
-      end
-
-      def save_parameter_to_cookie(name, value)
-        hash = rparam_cookie
-        hash[name] = value
-        cookies.permanent[:parameters] = {
-          value: hash.to_json,
-          path: request.path,
-          httponly: true,
-        }
-      end
-
-      def load_parameter(name, options)
-        user = current_rparam_user
-        if user.nil?
-          value = rparam_cookie[name]
-        else
-          rparam_memory = user.rparam_memories.find_by(
-            action: full_action_name,
-            name: name,
-          )
-          if rparam_memory.nil?
-            return nil
-          end
-          value = rparam_memory.value
+          return rparam_cookie
         end
 
-        if options[:save] == :relative_date
-          difference = Parser.parse_int(value)
-          if difference.nil?
-            value = ''
-          else
-            date = Time.zone.today + difference
-            value = date.strftime '%F'
-          end
+        rparam_memory = user.rparam_memories.find_by(
+          action: full_action_name,
+        )
+        if rparam_memory.nil?
+          return nil
         end
-
-        value
+        JSON.parse(rparam_memory.value, symbolize_names: true)
+      rescue
+        nil
       end
 
       def rparam_cookie
         JSON.parse(cookies[:parameters], symbolize_names: true)
       rescue
-        {}
+        nil
+      end
+
+      def save_rparam_memory(memory)
+        user = current_rparam_user
+        if user.nil?
+          cookies.permanent[:parameters] = {
+            value: hash.to_json,
+            path: request.path,
+            httponly: true,
+          }
+        else
+          rparam_memory = user.rparam_memories.find_or_create_by(
+            action: full_action_name,
+          )
+          rparam_memory.update(value: memory.to_json)
+        end
       end
 
       def current_rparam_user
--- test/dummy/spec/lib/controller_spec.rb
+++ test/dummy/spec/lib/controller_spec.rb
@@ -24,290 +24,290 @@ RSpec.describe TestsController, type: :controller do
 
   end
 
-  describe 'apply_each_rparam' do
+  # describe 'apply_each_rparam' do
 
-    describe 'inclusion' do
+  #   describe 'inclusion' do
 
-      example do
-        get :index, params: { order: 'asc' }
-        @controller.apply_each_rparam :order, inclusion: %w(asc desc)
-        expect(@controller.params[:order]).to eq 'asc'
+  #     example do
+  #       get :index, params: { order: 'asc' }
+  #       @controller.apply_each_rparam :order, inclusion: %w(asc desc)
+  #       expect(@controller.params[:order]).to eq 'asc'
 
-        get :index, params: { order: 'foo' }
-        @controller.apply_each_rparam :order, inclusion: %w(asc desc)
-        expect(@controller.params[:order]).to eq nil
-      end
+  #       get :index, params: { order: 'foo' }
+  #       @controller.apply_each_rparam :order, inclusion: %w(asc desc)
+  #       expect(@controller.params[:order]).to eq nil
+  #     end
 
-      describe 'with default value' do
+  #     describe 'with default value' do
 
-        example do
-          get :index, params: { order: 'asc' }
-          @controller.apply_each_rparam :order, inclusion: %w(asc desc), default: 'asc'
-          expect(@controller.params[:order]).to eq 'asc'
+  #       example do
+  #         get :index, params: { order: 'asc' }
+  #         @controller.apply_each_rparam :order, inclusion: %w(asc desc), default: 'asc'
+  #         expect(@controller.params[:order]).to eq 'asc'
 
-          get :index, params: { order: 'foo' }
-          @controller.apply_each_rparam :order, inclusion: %w(asc desc), default: 'asc'
-          expect(@controller.params[:order]).to eq 'asc'
-        end
+  #         get :index, params: { order: 'foo' }
+  #         @controller.apply_each_rparam :order, inclusion: %w(asc desc), default: 'asc'
+  #         expect(@controller.params[:order]).to eq 'asc'
+  #       end
 
-      end
+  #     end
 
-    end
+  #   end
 
-    describe 'exclusion' do
+  #   describe 'exclusion' do
 
-      example do
-        get :index, params: { value: 'a' }
-        @controller.apply_each_rparam :value, exclusion: 'a'
-        expect(@controller.params[:value]).to eq nil
+  #     example do
+  #       get :index, params: { value: 'a' }
+  #       @controller.apply_each_rparam :value, exclusion: 'a'
+  #       expect(@controller.params[:value]).to eq nil
 
-        get :index, params: { value: 'a' }
-        @controller.apply_each_rparam :value, exclusion: %w(a b c)
-        expect(@controller.params[:value]).to eq nil
+  #       get :index, params: { value: 'a' }
+  #       @controller.apply_each_rparam :value, exclusion: %w(a b c)
+  #       expect(@controller.params[:value]).to eq nil
 
-        get :index, params: { value: 'd' }
-        @controller.apply_each_rparam :value, exclusion: %w(a b c)
-        expect(@controller.params[:value]).to eq 'd'
-      end
+  #       get :index, params: { value: 'd' }
+  #       @controller.apply_each_rparam :value, exclusion: %w(a b c)
+  #       expect(@controller.params[:value]).to eq 'd'
+  #     end
 
-    end
+  #   end
 
-    describe 'type: Date' do
+  #   describe 'type: Date' do
 
-      example 'date' do
-        get :index, params: { date: '2018-05-15' }
-        @controller.apply_each_rparam :date, type: Date
-        expect(@controller.params[:date]).to eq Date.new(2018, 5, 15)
-      end
+  #     example 'date' do
+  #       get :index, params: { date: '2018-05-15' }
+  #       @controller.apply_each_rparam :date, type: Date
+  #       expect(@controller.params[:date]).to eq Date.new(2018, 5, 15)
+  #     end
 
-      example 'month' do
-        get :index, params: { date: '2018-05' }
-        @controller.apply_each_rparam :date, type: Date
-        expect(@controller.params[:date]).to eq Date.new(2018, 5, 1)
+  #     example 'month' do
+  #       get :index, params: { date: '2018-05' }
+  #       @controller.apply_each_rparam :date, type: Date
+  #       expect(@controller.params[:date]).to eq Date.new(2018, 5, 1)
 
-        get :index, params: { date: '2018-5' }
-        @controller.apply_each_rparam :date, type: Date
-        expect(@controller.params[:date]).to eq Date.new(2018, 5, 1)
-      end
+  #       get :index, params: { date: '2018-5' }
+  #       @controller.apply_each_rparam :date, type: Date
+  #       expect(@controller.params[:date]).to eq Date.new(2018, 5, 1)
+  #     end
 
-      example 'year' do
-        get :index, params: { date: '2018' }
-        @controller.apply_each_rparam :date, type: Date
-        expect(@controller.params[:date]).to eq Date.new(2018, 1, 1)
-      end
+  #     example 'year' do
+  #       get :index, params: { date: '2018' }
+  #       @controller.apply_each_rparam :date, type: Date
+  #       expect(@controller.params[:date]).to eq Date.new(2018, 1, 1)
+  #     end
 
-      example 'invalid date' do
-        get :index, params: { date: 'invalid' }
-        @controller.apply_each_rparam :date, type: Date
-        expect(@controller.params[:date]).to eq nil
-      end
+  #     example 'invalid date' do
+  #       get :index, params: { date: 'invalid' }
+  #       @controller.apply_each_rparam :date, type: Date
+  #       expect(@controller.params[:date]).to eq nil
+  #     end
 
-      example 'with default value' do
-        today = Date.today
-        get :index
-        @controller.apply_each_rparam :value, type: Date, default: today
-        expect(@controller.params[:value]).to eq today
+  #     example 'with default value' do
+  #       today = Date.today
+  #       get :index
+  #       @controller.apply_each_rparam :value, type: Date, default: today
+  #       expect(@controller.params[:value]).to eq today
 
-        get :index
-        @controller.apply_each_rparam :value, type: Date, default: 7.days.ago
-        expect(@controller.params[:value]).to eq today - 7
+  #       get :index
+  #       @controller.apply_each_rparam :value, type: Date, default: 7.days.ago
+  #       expect(@controller.params[:value]).to eq today - 7
 
-        get :index, params: { value: '' }
-        @controller.apply_each_rparam :value, type: Date, default: today
-        expect(@controller.params[:value]).to eq nil
-      end
+  #       get :index, params: { value: '' }
+  #       @controller.apply_each_rparam :value, type: Date, default: today
+  #       expect(@controller.params[:value]).to eq nil
+  #     end
 
-      example 'relative date with default value' do
-        today = Date.today
-        get :index
-        @controller.apply_each_rparam :value, type: Date, save: :relative_date, default: today
-        expect(@controller.params[:value]).to eq today
-      end
-    end
+  #     example 'relative date with default value' do
+  #       today = Date.today
+  #       get :index
+  #       @controller.apply_each_rparam :value, type: Date, save: :relative_date, default: today
+  #       expect(@controller.params[:value]).to eq today
+  #     end
+  #   end
 
-    describe 'type: Array' do
+  #   describe 'type: Array' do
 
-      example 'array' do
-        get :index, params: { value: %w(a b c) }
-        @controller.apply_each_rparam :value, type: Array
-        expect(@controller.params[:value]).to eq %w(a b c)
-      end
+  #     example 'array' do
+  #       get :index, params: { value: %w(a b c) }
+  #       @controller.apply_each_rparam :value, type: Array
+  #       expect(@controller.params[:value]).to eq %w(a b c)
+  #     end
 
-      example 'not array' do
-        get :index, params: { value: 'invalid' }
-        @controller.apply_each_rparam :value, type: Array
-        expect(@controller.params[:value]).to eq []
-      end
+  #     example 'not array' do
+  #       get :index, params: { value: 'invalid' }
+  #       @controller.apply_each_rparam :value, type: Array
+  #       expect(@controller.params[:value]).to eq []
+  #     end
 
-      example 'without parameter' do
-        get :index
-        @controller.apply_each_rparam :value, type: Array
-        expect(@controller.params[:value]).to eq []
-      end
+  #     example 'without parameter' do
+  #       get :index
+  #       @controller.apply_each_rparam :value, type: Array
+  #       expect(@controller.params[:value]).to eq []
+  #     end
 
-      example 'with inclusion' do
-        get :index, params: { value: %w(a b c) }
-        @controller.apply_each_rparam :value, type: Array, inclusion: 'a'
-        expect(@controller.params[:value]).to eq %w(a)
+  #     example 'with inclusion' do
+  #       get :index, params: { value: %w(a b c) }
+  #       @controller.apply_each_rparam :value, type: Array, inclusion: 'a'
+  #       expect(@controller.params[:value]).to eq %w(a)
 
-        get :index, params: { value: %w(a b c) }
-        @controller.apply_each_rparam :value, type: Array, inclusion: %w(b c d)
-        expect(@controller.params[:value]).to eq %w(b c)
-      end
+  #       get :index, params: { value: %w(a b c) }
+  #       @controller.apply_each_rparam :value, type: Array, inclusion: %w(b c d)
+  #       expect(@controller.params[:value]).to eq %w(b c)
+  #     end
 
-      example 'with exclusion' do
-        get :index, params: { value: %w(a b c) }
-        @controller.apply_each_rparam :value, type: Array, exclusion: 'a'
-        expect(@controller.params[:value]).to eq %w(b c)
+  #     example 'with exclusion' do
+  #       get :index, params: { value: %w(a b c) }
+  #       @controller.apply_each_rparam :value, type: Array, exclusion: 'a'
+  #       expect(@controller.params[:value]).to eq %w(b c)
 
-        get :index, params: { value: %w(a b c) }
-        @controller.apply_each_rparam :value, type: Array, exclusion: %w(b c d)
-        expect(@controller.params[:value]).to eq %w(a)
+  #       get :index, params: { value: %w(a b c) }
+  #       @controller.apply_each_rparam :value, type: Array, exclusion: %w(b c d)
+  #       expect(@controller.params[:value]).to eq %w(a)
 
-        get :index, params: { value: [nil, 1] }
-        @controller.apply_each_rparam :value, type: Array, exclusion: ''
-        expect(@controller.params[:value]).to eq ['1']
-      end
+  #       get :index, params: { value: [nil, 1] }
+  #       @controller.apply_each_rparam :value, type: Array, exclusion: ''
+  #       expect(@controller.params[:value]).to eq ['1']
+  #     end
 
-      example 'with default value' do
-        get :index
-        @controller.apply_each_rparam :value, type: Array, default: %w(a b c)
-        expect(@controller.params[:value]).to eq %w(a b c)
-      end
+  #     example 'with default value' do
+  #       get :index
+  #       @controller.apply_each_rparam :value, type: Array, default: %w(a b c)
+  #       expect(@controller.params[:value]).to eq %w(a b c)
+  #     end
 
-    end
+  #   end
 
-    describe 'save' do
+  #   describe 'save' do
 
-      example 'true' do
-        get :index, params: { order: 'asc' }
-        @controller.apply_each_rparam :order, save: true
-        expect(@controller.params[:order]).to eq 'asc'
-        expect(RparamMemory.count).to eq 0
+  #     example 'true' do
+  #       get :index, params: { order: 'asc' }
+  #       @controller.apply_each_rparam :order, save: true
+  #       expect(@controller.params[:order]).to eq 'asc'
+  #       expect(RparamMemory.count).to eq 0
 
-        get :index
-        @controller.apply_each_rparam :order, save: true
-        expect(@controller.params[:order]).to eq 'asc'
-        expect(RparamMemory.count).to eq 0
-      end
+  #       get :index
+  #       @controller.apply_each_rparam :order, save: true
+  #       expect(@controller.params[:order]).to eq 'asc'
+  #       expect(RparamMemory.count).to eq 0
+  #     end
 
-      example 'false' do
-        get :index, params: { order: 'asc' }
-        @controller.apply_each_rparam :order, save: false
-        expect(@controller.params[:order]).to eq 'asc'
-        expect(RparamMemory.count).to eq 0
+  #     example 'false' do
+  #       get :index, params: { order: 'asc' }
+  #       @controller.apply_each_rparam :order, save: false
+  #       expect(@controller.params[:order]).to eq 'asc'
+  #       expect(RparamMemory.count).to eq 0
 
-        get :index
-        @controller.apply_each_rparam :order, save: false
-        expect(@controller.params[:order]).to eq nil
-        expect(RparamMemory.count).to eq 0
-      end
+  #       get :index
+  #       @controller.apply_each_rparam :order, save: false
+  #       expect(@controller.params[:order]).to eq nil
+  #       expect(RparamMemory.count).to eq 0
+  #     end
 
-    end
+  #   end
 
-    describe 'login user' do
+  #   describe 'login user' do
 
-      let!(:user){ User.create }
-      before{ allow(@controller).to receive(:current_user).and_return(user) }
+  #     let!(:user){ User.create }
+  #     before{ allow(@controller).to receive(:current_user).and_return(user) }
 
-      describe 'save' do
+  #     describe 'save' do
 
-        example 'true' do
-          get :index, params: { order: 'asc' }
-          @controller.apply_each_rparam :order, save: true
-          expect(@controller.params[:order]).to eq 'asc'
-          expect(RparamMemory.count).to eq 1
+  #       example 'true' do
+  #         get :index, params: { order: 'asc' }
+  #         @controller.apply_each_rparam :order, save: true
+  #         expect(@controller.params[:order]).to eq 'asc'
+  #         expect(RparamMemory.count).to eq 1
 
-          get :index
-          @controller.apply_each_rparam :order, save: true
-          expect(@controller.params[:order]).to eq 'asc'
-          expect(RparamMemory.count).to eq 1
-        end
+  #         get :index
+  #         @controller.apply_each_rparam :order, save: true
+  #         expect(@controller.params[:order]).to eq 'asc'
+  #         expect(RparamMemory.count).to eq 1
+  #       end
 
-        example 'false' do
-          get :index, params: { order: 'asc' }
-          @controller.apply_each_rparam :order, save: false
-          expect(@controller.params[:order]).to eq 'asc'
-          expect(RparamMemory.count).to eq 0
+  #       example 'false' do
+  #         get :index, params: { order: 'asc' }
+  #         @controller.apply_each_rparam :order, save: false
+  #         expect(@controller.params[:order]).to eq 'asc'
+  #         expect(RparamMemory.count).to eq 0
 
-          get :index
-          @controller.apply_each_rparam :order, save: false
-          expect(@controller.params[:order]).to eq nil
-          expect(RparamMemory.count).to eq 0
-        end
+  #         get :index
+  #         @controller.apply_each_rparam :order, save: false
+  #         expect(@controller.params[:order]).to eq nil
+  #         expect(RparamMemory.count).to eq 0
+  #       end
 
-      end
+  #     end
 
-      describe 'save array' do
-        example do
-          get :index, params: { value: %w(a b c) }
-          @controller.apply_each_rparam :value, type: Array, save: true
-          expect(@controller.params[:value]).to eq %w(a b c)
-          expect(RparamMemory.count).to eq 1
-          expect(RparamMemory.first.value).to eq %w(a b c).to_s
+  #     describe 'save array' do
+  #       example do
+  #         get :index, params: { value: %w(a b c) }
+  #         @controller.apply_each_rparam :value, type: Array, save: true
+  #         expect(@controller.params[:value]).to eq %w(a b c)
+  #         expect(RparamMemory.count).to eq 1
+  #         expect(RparamMemory.first.value).to eq %w(a b c).to_s
 
-          get :index
-          @controller.apply_each_rparam :value, type: Array, save: true
-          expect(@controller.params[:value]).to eq %w(a b c)
-        end
-      end
+  #         get :index
+  #         @controller.apply_each_rparam :value, type: Array, save: true
+  #         expect(@controller.params[:value]).to eq %w(a b c)
+  #       end
+  #     end
 
-      describe 'save relative date' do
+  #     describe 'save relative date' do
 
-        example do
-          travel_to Date.new(2018, 10, 15)
-          get :index, params: { date: '2018-10-20' }
-          @controller.apply_each_rparam :date, save: :relative_date
-          expect(@controller.params[:date]).to eq '2018-10-20'
-          expect(RparamMemory.count).to eq 1
-          expect(RparamMemory.first.value).to eq '5'
+  #       example do
+  #         travel_to Date.new(2018, 10, 15)
+  #         get :index, params: { date: '2018-10-20' }
+  #         @controller.apply_each_rparam :date, save: :relative_date
+  #         expect(@controller.params[:date]).to eq '2018-10-20'
+  #         expect(RparamMemory.count).to eq 1
+  #         expect(RparamMemory.first.value).to eq '5'
 
-          get :index
-          @controller.apply_each_rparam :date, save: :relative_date
-          expect(@controller.params[:date]).to eq '2018-10-20'
+  #         get :index
+  #         @controller.apply_each_rparam :date, save: :relative_date
+  #         expect(@controller.params[:date]).to eq '2018-10-20'
 
-          travel 5.days
-          get :index
-          @controller.apply_each_rparam :date, save: :relative_date
-          expect(@controller.params[:date]).to eq '2018-10-25'
-        end
+  #         travel 5.days
+  #         get :index
+  #         @controller.apply_each_rparam :date, save: :relative_date
+  #         expect(@controller.params[:date]).to eq '2018-10-25'
+  #       end
 
-        example 'blank' do
-          get :index, params: { date: '' }
-          @controller.apply_each_rparam :date, save: :relative_date
-          expect(@controller.params[:date]).to eq ''
-          expect(RparamMemory.count).to eq 1
-          expect(RparamMemory.first.value).to eq nil
+  #       example 'blank' do
+  #         get :index, params: { date: '' }
+  #         @controller.apply_each_rparam :date, save: :relative_date
+  #         expect(@controller.params[:date]).to eq ''
+  #         expect(RparamMemory.count).to eq 1
+  #         expect(RparamMemory.first.value).to eq nil
 
-          get :index
-          @controller.apply_each_rparam :date, save: :relative_date
-          expect(@controller.params[:date]).to eq ''
-        end
+  #         get :index
+  #         @controller.apply_each_rparam :date, save: :relative_date
+  #         expect(@controller.params[:date]).to eq ''
+  #       end
 
-        example 'without params' do
-          get :index
-          @controller.apply_each_rparam :date, save: :relative_date
-          expect(@controller.params[:date]).to eq nil
-          expect(RparamMemory.count).to eq 0
-        end
+  #       example 'without params' do
+  #         get :index
+  #         @controller.apply_each_rparam :date, save: :relative_date
+  #         expect(@controller.params[:date]).to eq nil
+  #         expect(RparamMemory.count).to eq 0
+  #       end
 
-        example 'invalid date' do
-          travel_to Date.new(2018, 10, 15)
-          get :index, params: { date: 'invalid' }
-          @controller.apply_each_rparam :date, save: :relative_date
-          # expect(@controller.params[:date]).to eq ''
-          expect(RparamMemory.count).to eq 1
-          expect(RparamMemory.first.value).to eq nil
+  #       example 'invalid date' do
+  #         travel_to Date.new(2018, 10, 15)
+  #         get :index, params: { date: 'invalid' }
+  #         @controller.apply_each_rparam :date, save: :relative_date
+  #         # expect(@controller.params[:date]).to eq ''
+  #         expect(RparamMemory.count).to eq 1
+  #         expect(RparamMemory.first.value).to eq nil
 
-          get :index
-          @controller.apply_each_rparam :date, save: :relative_date
-          expect(@controller.params[:date]).to eq ''
-        end
+  #         get :index
+  #         @controller.apply_each_rparam :date, save: :relative_date
+  #         expect(@controller.params[:date]).to eq ''
+  #       end
 
-      end
+  #     end
 
-    end
+  #   end
 
-  end
+  # end
 
 end
