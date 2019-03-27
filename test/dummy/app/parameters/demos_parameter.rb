ef78ce3a76a757d6403a8110770ee9a0d5287ec9
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-03-27 14:10:39 +0900

    Add integer input to demo page

--- test/dummy/app/parameters/demos_parameter.rb
+++ test/dummy/app/parameters/demos_parameter.rb
@@ -5,6 +5,7 @@ class DemosParameter < Rparam::Parameter
   def show
     param :date, type: Date, save: :relative_date, default: 1.days.ago
     param :month, save: :relative_month, default: Time.zone.today
+    param :integer, type: Integer, save: true, min: 1, max: 10
     param :checkbox, type: Array, save: true, exclusion: '', default: %w(value1)
   end
 
