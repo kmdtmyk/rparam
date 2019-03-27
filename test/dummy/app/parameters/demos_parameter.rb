8d2576149288060c1d4388ebd190b588a3a80042
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-03-27 12:02:21 +0900

    Add input month to demo page

--- test/dummy/app/parameters/demos_parameter.rb
+++ test/dummy/app/parameters/demos_parameter.rb
@@ -3,8 +3,8 @@
 class DemosParameter < Rparam::Parameter
 
   def show
-    param :from_date, type: Date, save: :relative_date, default: 3.days.ago
-    param :to_date, type: Date, save: :relative_date
+    param :date, type: Date, save: :relative_date, default: 1.days.ago
+    param :month, save: :relative_month, default: Time.zone.today
     param :checkbox, type: Array, save: true, exclusion: '', default: %w(value1)
   end
 
